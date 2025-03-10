2f7eebc354610de04ec6cdcfd3d38b8df6beb861
HaydarChan <atqiyahaydar15@gmail.com>
2025-03-10 22:52:42 +0700

    feat: semoga selesai

--- components/ui/separator.tsx
+++ components/ui/separator.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import * as React from "react"
+// @ts-expect-error ini emang error kochak
 import * as SeparatorPrimitive from "@radix-ui/react-separator"
 
 import { cn } from "@/lib/utils"
